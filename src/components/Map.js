a500aa5a07de37ed2542ab72d8694857c019a77e
Shenelle_Peart <speart@limoanywhere.com>
2020-11-24 15:29:18 -0600

    created location info box omponent

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,18 +1,25 @@
+import { useState } from "react";
 import GoogleMapReact from "google-map-react";
 import LocationMarker from "./LocationMarker";
+import LocationInfoBox from "./LocationInfoBox";
 
 const Map = ({ center, zoom, eventData }) => {
-  const markers = eventData.map((ev) => {
-    if (ev.categories[0].id === 8) {
-      return (
-        <LocationMarker
-          lat={ev.geometries[0].coordinates[1]}
-          lng={ev.geometries[0].coordinates[0]}
-        />
-      );
-    }
-    return null;
-  });
+    const [locationInfo, setLocationInfo] = useState(null);
+
+    const markers = eventData.map(ev =>{
+        if (ev.categories[0].id === 8) {
+            return (
+              <LocationMarker
+                lat={ev.geometries[0].coordinates[1]}
+                lng={ev.geometries[0].coordinates[0]}
+                onClick={()=> setLocationInfo({id: ev.id, title: ev.title})}
+              />
+            );
+          }
+          return null;
+    })
+  
+  
 
   return (
     <div className="map">
@@ -25,6 +32,7 @@ const Map = ({ center, zoom, eventData }) => {
       >
         {markers}
       </GoogleMapReact>
+      {locationInfo && <LocationInfoBox info={locationInfo} />}
     </div>
   );
 };
